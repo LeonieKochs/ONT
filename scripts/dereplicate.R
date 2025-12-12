558fd4532bd94acf2b5ce2e1f83faf6819192ff6
LeonieKochs <leonie.kochs@web.de>
2025-12-12 09:52:52 +0100

    output dir

--- scripts/dereplicate.R
+++ scripts/dereplicate.R
@@ -4,5 +4,7 @@ library(dada2)
 fn <- snakemake@input[[1]]
 out <- snakemake@output[[1]]
 
+dir.create(dirname(out), recursive = TRUE, showWarnings = FALSE)
+
 drp <- derepFastq(fn)
 saveRDS(drp, out)
